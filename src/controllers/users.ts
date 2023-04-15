32835b5380a76825f9f6f01112bad0f800bb1077
Adnan <adnan.silajdzic.it@gmail.com>
2023-04-15 11:40:54 +0200

    added jwt tokens to sign in function

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,46 +1,73 @@
-import { RequestHandler } from 'express';
-import mongoose from 'mongoose';
-import createHttpError from 'http-errors';
-import bcrypt from 'bcrypt';
-import { UserModel, UserRole } from '../models/user';
+import { RequestHandler } from "express";
+import mongoose from "mongoose";
+import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
+import { UserModel, UserRole } from "../models/user";
+import jwt from "jsonwebtoken";
+import env from "../utils/validate-env";
 
 interface RegisterUserBody {
-	email?: string;
-	password?: string;
-	name?: string;
-	role?: UserRole;
+  email?: string;
+  password?: string;
+  name?: string;
+  role?: UserRole;
 }
 
 interface LoginUserBody {
-	email?: string;
-	password?: string;
+  email?: string;
+  password?: string;
+  rememberMe?: boolean;
 }
 
-export const registerUser: RequestHandler<unknown, unknown, RegisterUserBody, unknown> = async (req, res, next) => {
-	const { email, password, name, role } = req.body;
-	try {
-		if (!email || !password || !name || !role) throw createHttpError(400, 'Missing required fields.');
-		const existingUser = await UserModel.findOne({ email });
-		if (existingUser) throw createHttpError(409, 'Email already registered.');
-		const saltRounds = 10;
-		const hashedPassword = await bcrypt.hash(password, saltRounds);
-		const newUser = await UserModel.create({ email, password: hashedPassword, name, role });
-		return res.status(201).json(newUser);
-	} catch (error) {
-		next(error);
-	}
+export const registerUser: RequestHandler<
+  unknown,
+  unknown,
+  RegisterUserBody,
+  unknown
+> = async (req, res, next) => {
+  const { email, password, name, role } = req.body;
+  try {
+    if (!email || !password || !name || !role)
+      throw createHttpError(400, "Missing required fields.");
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) throw createHttpError(409, "Email already registered.");
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = await UserModel.create({
+      email,
+      password: hashedPassword,
+      name,
+      role,
+    });
+    return res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const loginUser: RequestHandler<unknown, unknown, LoginUserBody, unknown> = async (req, res, next) => {
-	const { email, password } = req.body;
-	try {
-		if (!email || !password) throw createHttpError(400, 'Missing required fields.');
-		const user = await UserModel.findOne({ email });
-		if (!user) throw createHttpError(401, 'Invalid email or password.');
-		const isPasswordValid = await bcrypt.compare(password, user.password);
-		if (!isPasswordValid) throw createHttpError(401, 'Invalid email or password.');
-		return res.status(200).json({ message: 'Login successful.' });
-	} catch (error) {
-		next(error);
-	}
+export const loginUser: RequestHandler<
+  unknown,
+  unknown,
+  LoginUserBody,
+  unknown
+> = async (req, res, next) => {
+  const { email, password, rememberMe } = req.body;
+  try {
+    if (!email || !password || rememberMe == null)
+      throw createHttpError(400, "Missing required fields.");
+    const user = await UserModel.findOne({ email });
+    if (!user) throw createHttpError(401, "Invalid email or password.");
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      throw createHttpError(401, "Invalid email or password.");
+
+    const expireLength = rememberMe ? "30d" : "1d";
+    const token = jwt.sign({ userId: user._id }, env.JWT_SECRET, {
+      expiresIn: expireLength,
+    });
+
+    return res.status(200).json({ token, expireLength });
+  } catch (error) {
+    next(error);
+  }
 };
--- src/utils/validate-env.ts
+++ src/utils/validate-env.ts
@@ -1,7 +1,8 @@
-import { cleanEnv } from 'envalid';
-import { port, str } from 'envalid/dist/validators';
+import { cleanEnv } from "envalid";
+import { port, str } from "envalid/dist/validators";
 
 export default cleanEnv(process.env, {
-	CONNECTION_STRING: str(),
-	PORT: port(),
+  CONNECTION_STRING: str(),
+  PORT: port(),
+  JWT_SECRET: str(),
 });
