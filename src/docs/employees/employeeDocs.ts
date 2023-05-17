3e0c266f4373416c994973896dc0e414b2f2bb49
Adnan <adnan.silajdzic.it@gmail.com>
2023-05-17 12:22:17 +0200

    seperated swagger docs from routes file

---
+++ src/docs/employees/employeeDocs.ts
@@ -0,0 +1,43 @@
+/**
+ * @swagger
+ *   /api/employees:
+ *   get:
+ *     description: Use to request current weather
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer token
+ *         required: true
+ *         type: string
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ *   /api/employees:
+ *   post:
+ *     description: Use to request current weather
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer token
+ *         required: true
+ *         type: string
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,28 +1,22 @@
-import swaggerJSDoc from "swagger-jsdoc";
-import { config } from "dotenv";
+import swaggerJsdoc from 'swagger-jsdoc';
+import { config } from 'dotenv';
 config();
 // Swagger definition
-const swaggerDefinition = {
-  openapi: "3.0.0",
-  info: {
-    title: "Project fire",
-    version: "1.0.0",
-    description: "API documentation for project fire",
-  },
-  servers: [
-    {
-      url: `http://localhost:${process.env.PORT}`, // Update with your server URL
-    },
-  ],
+const swaggerOptions = {
+	swaggerDefinition: {
+		info: {
+			title: 'Project Fire API',
+			description: 'Documentation for project fire API',
+			version: '1.0.0',
+			contact: {
+				name: 'Ant Colony',
+			},
+			servers: [`http://localhost:${process.env.PORT}`],
+		},
+	},
+	apis: ['./src/docs/employees/*.ts'],
 };
 
-// Options for the swagger-jsdoc plugin
-const options = {
-  swaggerDefinition,
-  apis: ["./routes/*.ts"], // Specify the path to your route files
-};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
-// Initialize swagger-jsdoc
-const swaggerSpec = swaggerJSDoc(options);
-
-export default swaggerSpec;
+export default swaggerDocs;
