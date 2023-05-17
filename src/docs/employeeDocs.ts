eeeb03bc0bd3e549a59b243dbd4a14fc9a4ecdc2
Adnan <adnan.silajdzic.it@gmail.com>
2023-05-17 13:52:35 +0200

    employee endpoints documented with swagger

--- src/docs/employeeDocs.ts
+++ src/docs/employeeDocs.ts
@@ -3,7 +3,7 @@
  *   /api/employees:
  *   get:
  *     tags: [employees]
- *     description:
+ *     description: This returns all employees from the database
  *     consumes:
  *       - application/json
  *     parameters:
@@ -23,10 +23,10 @@
 
 /**
  * @swagger
- *   /api/employees:
- *   post:
+ *   /api/employees/{employeeId}:
+ *   get:
  *     tags: [employees]
- *     description:
+ *     description: This returns a specific employee from the databse
  *     consumes:
  *       - application/json
  *     parameters:
@@ -34,6 +34,81 @@
  *         name: Authorization
  *         description: Bearer token
  *         required: true
+ *         type: string
+ *       - in: path
+ *         name: employeeId
+ *         type: string
+ *         required: true
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ *   /api/employees/add:
+ *   post:
+ *     tags: [employees]
+ *     description: This creates an employee in the databse
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer token
+ *         required: true
+ *         type: string
+ *       - in: body
+ *         name: request
+ *         description: Request body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             firstName:
+ *               type: string
+ *               example: Adnan
+ *             lastName:
+ *               type: string
+ *               example: Silajdzic
+ *             salary:
+ *               type: number
+ *               example: 4000
+ *             department:
+ *               type: string
+ *               example: Mobile
+ *             techStack:
+ *               type: string[]
+ *               example: [react, node]
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ *   /api/employees/{employeeId}:
+ *   delete:
+ *     tags: [employees]
+ *     description: This deletes a specific employee from the databse
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer token
+ *         required: true
+ *         type: string
+ *       - in: path
+ *         name: employeeId
+ *         description: Id of the employee you wish to delete
  *         type: string
  *     responses:
  *       '200':
